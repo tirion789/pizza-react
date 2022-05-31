355c08945acc0eae022b561492fbdeaf168854f9
tirion <tirionchen@mail.ru>
2022-05-31 16:28:50 +0300

    components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import Header from './components/Header';
+
+import Cotegories from './components/Categories';
+
+import Sort from './components/Sort';
+import PizzaBlock from './components/Pizza-block';
+
+import './scss/app.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="wrapper">
+      <Header />
+      <div class="content">
+        <div class="container">
+          <div class="content__top">
+            <Cotegories />
+            <Sort />
+          </div>
+          <h2 class="content__title">Все пиццы</h2>
+          <div class="content__items">
+            <PizzaBlock title="Мексиканская" price="500" />
+            <PizzaBlock title="Итальянская" price="450" />
+            <PizzaBlock title="Охотничая" price="550" />
+            <PizzaBlock />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
