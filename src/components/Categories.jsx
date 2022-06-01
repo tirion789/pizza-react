5d87fc6733993a6f31a219feb425e82cfe7130a9
tirion <tirionchen@mail.ru>
2022-06-01 18:28:00 +0300

    destructurization, React Hooks, onClick?

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,8 +1,8 @@
 const Cotegories = () => {
     return (
-      <div class="categories">
+      <div className="categories">
         <ul>
-          <li class="active">Все</li>
+          <li className="active">Все</li>
           <li>Мясные</li>
           <li>Вегетарианская</li>
           <li>Гриль</li>
--- src/components/Pizza-block.jsx
+++ src/components/Pizza-block.jsx
@@ -1,25 +1,36 @@
-const PizzaBlock = (props) => {
-    return (<div class="pizza-block">
+import React from "react";
+
+const PizzaBlock = ({title, price}) => {
+  const [pizzaCount, setPizzaCount] = React.useState(0);
+
+  const onClickAddBtn = () => {
+    setPizzaCount(pizzaCount + 1)
+  }
+
+  const  onClickResetBtn = () => {
+    setPizzaCount(React.initialState = 0);
+  }
+    return (<div className="pizza-block">
     <img
-      class="pizza-block__image"
+      className="pizza-block__image"
       src="https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"
       alt="Pizza"
     />
-    <h4 class="pizza-block__title">{props.title}</h4>
-    <div class="pizza-block__selector">
+    <h4 className="pizza-block__title">{title}</h4>
+    <div className="pizza-block__selector">
       <ul>
-        <li class="active">тонкое</li>
+        <li className="active">тонкое</li>
         <li>традиционное</li>
       </ul>
       <ul>
-        <li class="active">26 см.</li>
+        <li className="active">26 см.</li>
         <li>30 см.</li>
         <li>40 см.</li>
       </ul>
     </div>
-    <div class="pizza-block__bottom">
-      <div class="pizza-block__price">от {props.price} ₽</div>
-      <div class="button button--outline button--add">
+    <div className="pizza-block__bottom">
+      <div className="pizza-block__price">от {price} ₽</div>
+      <button onClick={onClickAddBtn} className="button button--outline button--add">
         <svg
           width="12"
           height="12"
@@ -32,9 +43,10 @@ const PizzaBlock = (props) => {
           />
         </svg>
         <span>Добавить</span>
-        <i>2</i>
-      </div>
+        <i>{pizzaCount}</i>
+      </button>
     </div>
+    <button className="button button--outline button--add button__reset"  onClick={onClickResetBtn} >Сбросить</button>
   </div>)
 }
 
